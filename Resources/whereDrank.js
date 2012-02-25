423a3c60d34d760188123b3bb85869c80b778b01
Grant Shipley <gshipley@gmail.com>
2012-02-24 22:20:14 -0500

    Cleanup

--- Resources/whereDrank.js
+++ Resources/whereDrank.js
@@ -55,31 +55,6 @@ function createDrankDetailWindow(e) {
 	});
 	
 	// Add our UI elements to our view
-	var beerAnnotation = Titanium.Map.createAnnotation({
-		latitude : 41.88925,
-		longitude : -87.632638,
-		title : e._title,
-		subtitle : e._when,
-		pincolor : Titanium.Map.ANNOTATION_RED,
-		animate : true
-	});
-
-	var mapview = Titanium.Map.createView({
-		top : 110,
-		height : 350,
-		mapType : Titanium.Map.STANDARD_TYPE,
-		region : {
-			latitude : 41.88925,
-			longitude : -87.632638,
-			latitudeDelta : 0.5,
-			longitudeDelta : 0.5
-		},
-		animate : true,
-		regionFit : true,
-		userLocation : true,
-		annotations:[beerAnnotation]
-	});
-	detailsView.add(mapview);
 
 	detailsView.add(labelBeerDetailName);
 	detailsView.add(labelWhenDrank);
