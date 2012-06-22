34ad84d7cf734869b53c949ac57ebde7f56b1c6c
Anthony Decena <anthony@mindelusions.com>
2012-06-22 12:14:20 -0700

    Cleaning up deprecated files. Consolidated formatting.

--- Resources/detailWindow.js
+++ Resources/detailWindow.js
@@ -1,84 +1,84 @@
 function DetailWindow(e) {
 	var windowBeerDetails = Ti.UI.createWindow({
-		top : 0,
-		left : 0,
-		backgroundColor : 'black',
-		title : L('winBeerDetails'),
-		barImage : 'images/navbar.png'
+		top: 0,
+		left: 0,
+		backgroundColor: 'black',
+		title: L('winBeerDetails'),
+		barImage: 'images/navbar.png'
 	});
 
 	var detailsView = Ti.UI.createView({
-		height : windowBeerDetails.height - 134,
-		left : 0,
-		top : 0,
-		backgroundColor : 'black',
-		borderRadius : 5
+		height: windowBeerDetails.height - 134,
+		left: 0,
+		top: 0,
+		backgroundColor: 'black',
+		borderRadius: 5
 	});
 
 	// Let start adding our UI elements
 	var beerImage = Titanium.UI.createImageView({
-		image : 'images/pitcher.jpg',
-		width : 80,
-		height : 62,
-		left : 10,
-		top : 20
+		image: 'images/pitcher.jpg',
+		width: 80,
+		height: 62,
+		left: 10,
+		top: 20
 	});
 
 	var labelBeerDetailName = Titanium.UI.createLabel({
-		width : 'auto',
-		height : 30,
-		top : 40,
-		left : 100,
-		color : 'white',
-		font : {
-			fontSize : 14,
-			fontFamily : 'Helvetica',
-			fontWeight : 'bold'
+		width: 'auto',
+		height: 30,
+		top: 40,
+		left: 100,
+		color: 'white',
+		font: {
+			fontSize: 14,
+			fontFamily: 'Helvetica',
+			fontWeight: 'bold'
 		},
-		text : e._title
+		text: e._title
 	});
 	var labelBreweryName = Titanium.UI.createLabel({
-		width : 'auto',
-		height : 30,
-		top : 58,
-		left : 100,
-		color : 'white',
-		font : {
-			fontSize : 14,
-			fontFamily : 'Helvetica',
-			fontWeight : 'bold'
+		width: 'auto',
+		height: 30,
+		top: 58,
+		left: 100,
+		color: 'white',
+		font: {
+			fontSize: 14,
+			fontFamily: 'Helvetica',
+			fontWeight: 'bold'
 		},
-		text : e._brewery
+		text: e._brewery
 	});
 
 	var textareaDescription = Titanium.UI.createTextArea({
-		color : 'white',
-		backgroundColor : 'black',
-		value : e._description,
-		height : 200,
-		width : '95%',
-		top : 105,
-		left : 10,
-		font : {
-			fontSize : 12,
-			fontFamily : 'Helvetica',
-			fontWeight : 'bold',
-			color : 'white'
+		color: 'white',
+		backgroundColor: 'black',
+		value: e._description,
+		height: 200,
+		width: '95%',
+		top: 105,
+		left: 10,
+		font: {
+			fontSize: 12,
+			fontFamily: 'Helvetica',
+			fontWeight: 'bold',
+			color: 'white'
 		},
-		borderColor : '#000',
-		editable : false
+		borderColor: '#000',
+		editable: false
 	});
 	var labelDrinkTheBeer = Titanium.UI.createLabel({
-		width : 'auto',
-		top : 320,
-		left : 115,
-		color : 'white',
-		font : {
-			fontSize : 26,
-			fontFamily : 'Helvetica',
-			fontWeight : 'bold'
+		width: 'auto',
+		top: 320,
+		left: 115,
+		color: 'white',
+		font: {
+			fontSize: 26,
+			fontFamily: 'Helvetica',
+			fontWeight: 'bold'
 		},
-		text : L('lblDrinkTheBeer')
+		text: L('lblDrinkTheBeer')
 	});
 
 	labelDrinkTheBeer.addEventListener('click', function(e) {
@@ -89,9 +89,9 @@ function DetailWindow(e) {
 		var postRequest = Ti.Network.createHTTPClient();
 		postRequest.open('POST', drinkPostURL);
 		postRequest.send({
-			username : Ti.App.Properties.getString('username'),
-			beerName : labelBeerDetailName.text,
-			when : dateFormat(now, "mmmm dS, yyyy, h:MM:ss TT"),
+			username: Ti.App.Properties.getString('username'),
+			beerName: labelBeerDetailName.text,
+			when: dateFormat(now, "mmmm dS, yyyy, h:MM:ss TT"),
 		});
 
 		tabGroup.setActiveTab(1);
